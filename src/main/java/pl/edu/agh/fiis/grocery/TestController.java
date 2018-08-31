4a2842af3d67f2d7aa512c77e11ee9cc29f82937
pwardynski <w.pwardynski@gmail.com>
2018-08-31 21:51:08 +0200

    GS6 - add SpringMVC with basic confguration and sample Controller

---
+++ src/main/java/pl/edu/agh/fiis/grocery/TestController.java
@@ -0,0 +1,17 @@
+package pl.edu.agh.fiis.grocery;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String get(Model model) {
+		model.addAttribute("msg", "TestMessage");
+		
+		return "test";
+	}
+}
